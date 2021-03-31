da3d7dfe68d89ee41127107a51e44b7d84c71984
Mael Jeuffrard <mj@ilatan.fr>
2021-03-31 19:41:57 +0200

    Ask for parameters instead of variables modifications

--- install.sh
+++ install.sh
@@ -6,7 +6,6 @@
 # Desc:
 #   - Install WireGuard without any configuration. Everything will be done through Wireguard-UI
 #   - Install WireGuard-UI
-#       - Wireguard is a very good project but not ended yet (security).
 #       - For a maximum security it will be use through ssh tunnel (ssh -L 5000:localhost:5000 user@vpn.domain.tld)
 #       - Please customise /opt/wgui/db/server/users.json after first login
 #   - Configure strict firewall
@@ -15,14 +14,15 @@
 #       - Allow Outgoing SSH, HTTPs, HTTP, DNS, Ping
 #       - Allow Ingoing SSH, Wireguard ($wg_port)
 #       - Allow everything needed by wireguard
-#   - Save iptables rules with iptables-persistent
+#   - Save iptables rules in /etc/iptables/
+#       - Load them at boot via /etc/network/if-up.d/iptables
 #
 # Sources:
 #   - Wireguard:
 #       - https://www.wireguard.com
 #       - https://github.com/WireGuard
 #   - Wireguard-ui:
-#       - https://github.com/ngoduykhanh/wireguard-ui 
+#       - https://github.com/ngoduykhanh/wireguard-ui
 #
 ###
 
@@ -37,23 +37,68 @@ if [ "$(lsb_release -is)" != "Debian" ] && [ "$(lsb_release -rs)" != "10" ]
     exit 1
 fi
 
-endpoint="server.domain.tld"
-wg_port="51820"
-wg_network="10.252.1.0/24"
-wg_interface="wg0"
-system_interface="ens18"
-ssh_port="22"
-admin_ip="client.domain.tld"
-wgui_link="https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.2.7/wireguard-ui-v0.2.7-linux-amd64.tar.gz"
-wgui_path="/opt/wgui"
-wgui_bin_path="/usr/local/bin"
-systemctl_path="/usr/bin/systemctl"
-deb_backport="deb https://deb.debian.org/debian/ buster-backports main"
+WGUI_LINK="https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.2.7/wireguard-ui-v0.2.7-linux-amd64.tar.gz"
+WGUI_PATH="/opt/wgui"
+WGUI_BIN_PATH="/usr/local/bin"
+SYSTEMCTL_PATH="/usr/bin/systemctl"
+BACKPORTS_REPO="deb https://deb.debian.org/debian/ buster-backports main"
 
+function main() {
+
+while [[ $ENDPOINT == "" ]]; do
+  echo "---"
+  read -p "Enpoint (IP or FQDN): " ENDPOINT
+done
+while ! [[ $WG_PORT =~ ^[0-9]+$ ]]; do
+  echo "---"
+  read -p "Wireguard port ? [51820]: " WG_PORT
+  WG_PORT=${WG_PORT:-"51820"}
+done
+while ! [[ $WG_NETWORK =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/[0-9]{1,2}$ ]]; do
+  echo "---"
+  read -p "Wireguard network ? [10.252.1.0/24]: " WG_NETWORK
+  WG_NETWORK=${WG_NETWORK:-"10.252.1.0/24"}
+done
+while [[ $WG_INTERFACE == "" ]]; do
+  echo "---"
+  read -p "Wireguard interface ? [wg0]: " WG_INTERFACE
+  WG_INTERFACE=${WG_INTERFACE:-"wg0"}
+done
+while [[ $SYS_INTERFACE == "" ]]; do
+  echo "---"
+  read -p "System network interface ? [eth0]: " SYS_INTERFACE
+  SYS_INTERFACE=${SYS_INTERFACE:-"eth0"}
+done
+while ! [[ $SSH_PORT =~ ^[0-9]+$ ]]; do
+  echo "---"
+  read -p "SSH port ? [22]: " SSH_PORT
+  SSH_PORT=${SSH_PORT:-"22"}
+done
+
+install
+network_conf
+firewall_conf
+wg_conf
+wgui_conf
+
+echo "########################################################################"
+echo "                            Setup done."
+echo ""
+echo "  - Your iptables rules was saved just in case in:"
+echo "      - /etc/iptables/rules.v4.bak"
+echo "      - /etc/iptables/rules.v6.bak"
+echo ""
+echo ""
+echo "  - To access your wireguard-ui please open a new ssh connexion"
+echo "      - ssh -L 5000:localhost:5000 user@myserver.domain.tld"
+echo "      - And browse to http://localhost:5000"
+echo ""
+echo "########################################################################"
+}
 
 function install() {
 
-  if ! grep -q "^$deb_backport" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+  if ! grep -q "^$BACKPORTS_REPO" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
     echo ""
     echo "### Enable debian-backports"
     echo "deb https://deb.debian.org/debian/ buster-backports main" >> /etc/apt/sources.list
@@ -69,10 +114,9 @@ function install() {
 
   echo ""
   echo "### Installing Wireguard-UI"
-  mkdir $wgui_path
-  umask 077 $wgui_path
-  wget -qO - $wgui_link | tar xzf - -C $wgui_path
-  ln -s $wgui_path/wireguard-ui $wgui_bin_path/wireguard-ui
+  mkdir -m 077 $WGUI_PATH
+  wget -qO - $WGUI_LINK | tar xzf - -C $WGUI_PATH
+  ln -s $WGUI_PATH/wireguard-ui $WGUI_BIN_PATH/wireguard-ui
 }
 
 function network_conf() {
@@ -83,66 +127,81 @@ function network_conf() {
 }
 
 function firewall_conf() {
+  echo ""
+  echo "### Firewall configuration"
+
   if [ "$(which iptables)" = "" ]; then
     echo ""
     echo "### iptables is required. Let's install it."
     apt install iptables -y
   fi
 
-  echo ""
-  echo "### Firewall IPV4 configuration"
-  iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -A INPUT -i lo -m comment --comment localhost-network -j ACCEPT
-  iptables -A INPUT -i $wg_interface -m comment --comment wireguard-network -j ACCEPT
-  iptables -A INPUT -p tcp -m tcp --dport $ssh_port -j ACCEPT
-  iptables -A INPUT -s $admin_ip -p icmp -m icmp --icmp-type 8 -m comment --comment "Ping-from-$admin_ip" -j ACCEPT
-  iptables -A INPUT -p udp -m udp --dport $wg_port -m comment --comment "external-port-wireguard" -j ACCEPT
-  iptables -A FORWARD -s $wg_network -i $wg_interface -o $system_interface -m comment --comment "Wireguard traffic from $wg_interface to $system_interface" -j ACCEPT
-  iptables -A FORWARD -d $wg_network -i $system_interface -o $wg_interface -m comment --comment "Wireguard traffic from $system_interface to $wg_interface" -j ACCEPT
-  iptables -A FORWARD -d $wg_network -i $wg_interface -o $wg_interface -m comment --comment "Wireguard traffic inside $wg_interface" -j ACCEPT
-  iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-  iptables -A OUTPUT -o lo -m comment --comment localhost-network -j ACCEPT
-  iptables -A OUTPUT -o $wg_interface -m comment --comment wireguard-network -j ACCEPT
-  iptables -A OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT
-  iptables -A OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT
-  iptables -A OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT
-  iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
-  iptables -A OUTPUT -p tcp -m tcp --dport 53 -j ACCEPT
-  iptables -A OUTPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT
-  iptables -t nat -A POSTROUTING -s $wg_network -o $system_interface -m comment --comment wireguard-nat-rule -j MASQUERADE
+  if ! [ -d /etc/iptables ]; then
+    mkdir -m 755 /etc/iptables
+  fi
+
+  /sbin/iptables-save > /etc/iptables/rules.v4.bak
+  /sbin/ip6tables-save > /etc/iptables/rules.v6.bak
+
+  RULES_4=(
+  "INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT"
+  "INPUT -i lo -m comment --comment localhost-network -j ACCEPT"
+  "INPUT -i $WG_INTERFACE -m comment --comment wireguard-network -j ACCEPT"
+  "INPUT -p tcp -m tcp --dport $SSH_PORT -j ACCEPT"
+  "INPUT -p icmp -m icmp --icmp-type 8 -m comment --comment Allow-ping -j ACCEPT"
+  "INPUT -p udp -m udp --dport $WG_PORT -m comment --comment external-port-wireguard -j ACCEPT"
+  "FORWARD -s $WG_NETWORK -i $WG_INTERFACE -o $SYS_INTERFACE -m comment --comment Wireguard-traffic-from-$WG_INTERFACE-to-$SYS_INTERFACE -j ACCEPT"
+  "FORWARD -d $WG_NETWORK -i $SYS_INTERFACE -o $WG_INTERFACE -m comment --comment Wireguard-traffic-from-$SYS_INTERFACE-to-$WG_INTERFACE -j ACCEPT"
+  "FORWARD -d $WG_NETWORK -i $WG_INTERFACE -o $WG_INTERFACE -m comment --comment Wireguard-traffic-inside-$WG_INTERFACE -j ACCEPT"
+  "OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT"
+  "OUTPUT -o lo -m comment --comment localhost-network -j ACCEPT"
+  "OUTPUT -o $WG_INTERFACE -m comment --comment wireguard-network -j ACCEPT"
+  "OUTPUT -p tcp -m tcp --dport 443 -j ACCEPT"
+  "OUTPUT -p tcp -m tcp --dport 80 -j ACCEPT"
+  "OUTPUT -p tcp -m tcp --dport 22 -j ACCEPT"
+  "OUTPUT -p udp -m udp --dport 53 -j ACCEPT"
+  "OUTPUT -p tcp -m tcp --dport 53 -j ACCEPT"
+  "OUTPUT -p icmp -m icmp --icmp-type 8 -j ACCEPT"
+  "POSTROUTING -t nat -s $WG_NETWORK -o $SYS_INTERFACE -m comment --comment wireguard-nat-rule -j MASQUERADE"
+  )
+
+  RULES_6=(
+  "INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT"
+  "INPUT -i lo -m comment --comment localhost-network -j ACCEPT"
+  "OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT"
+  "OUTPUT -o lo -m comment --comment localhost-network -j ACCEPT"
+  )
+
+  for e in "${RULES_4[@]}"; do
+    iptables -C $e > /dev/null 2>&1 || iptables -A $e
+  done
+  for e in "${RULES_6[@]}"; do
+    ip6tables -C $e > /dev/null 2>&1 || ip6tables -A $e
+  done
+
   iptables -P INPUT DROP
   iptables -P FORWARD DROP
   iptables -P OUTPUT DROP
-
-  echo ""
-  echo "### Firewall IPV6 configuration"
-  ip6tables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-  ip6tables -A INPUT -i lo -m comment --comment localhost-network -j ACCEPT
-  ip6tables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
-  ip6tables -A OUTPUT -o lo -m comment --comment localhost-network -j ACCEPT
   ip6tables -P INPUT DROP
   ip6tables -P FORWARD DROP
   ip6tables -P OUTPUT DROP
 
-  if [ "$(which netfilter-persistent)" = "" ]; then
-    echo ""
-    echo "### iptables-persistent is required. Let's install it."
-    echo "iptables-persistent iptables-persistent/autosave_v4	boolean	true" | debconf-set-selections
-    echo "iptables-persistent iptables-persistent/autosave_v6	boolean	true" | debconf-set-selections
-    apt install iptables-persistent -y
-  fi
-  
-  echo ""
-  echo "### Saving firewall rules"
-  service netfilter-persistent save
+  /sbin/iptables-save > /etc/iptables/rules.v4
+  /sbin/ip6tables-save > /etc/iptables/rules.v6
+
+  echo "#!/bin/sh
+  /sbin/iptables-restore < /etc/iptables/rules.v4
+  /sbin/ip6tables-restore < /etc/iptables/rules.v6" > /etc/network/if-up.d/iptables
+  chmod 755 /etc/network/if-up.d/iptables
+
 }
 
 function wg_conf() {
   echo ""
   echo "### Making default Wireguard conf"
   umask 077 /etc/wireguard/
-  touch /etc/wireguard/$wg_interface.conf
-  systemctl enable wg-quick@$wg_interface.service
+  touch /etc/wireguard/$WG_INTERFACE.conf
+  systemctl enable wg-quick@$WG_INTERFACE.service
 }
 
 function wgui_conf() {
@@ -155,8 +214,8 @@ function wgui_conf() {
 
   [Service]
   Type=simple
-  WorkingDirectory=$wgui_path
-  ExecStart=$wgui_bin_path/wireguard-ui
+  WorkingDirectory=$WGUI_PATH
+  ExecStart=$WGUI_BIN_PATH/wireguard-ui
 
   [Install]
   WantedBy=multi-user.target" > /etc/systemd/system/wgui_http.service
@@ -170,13 +229,13 @@ function wgui_conf() {
 
   [Service]
   Type=oneshot
-  ExecStart=$systemctl_path restart wg-quick@$wg_interface.service" > /etc/systemd/system/wgui.service
+  ExecStart=$SYSTEMCTL_PATH restart wg-quick@$WG_INTERFACE.service" > /etc/systemd/system/wgui.service
 
   echo "[Unit]
-  Description=Watch /etc/wireguard/$wg_interface.conf for changes
+  Description=Watch /etc/wireguard/$WG_INTERFACE.conf for changes
 
   [Path]
-  PathModified=/etc/wireguard/$wg_interface.conf
+  PathModified=/etc/wireguard/$WG_INTERFACE.conf
 
   [Install]
   WantedBy=multi-user.target" > /etc/systemd/system/wgui.path
@@ -184,9 +243,4 @@ function wgui_conf() {
   systemctl enable wgui.{path,service}
   systemctl start wgui.{path,service}
 }
-
-install
-network_conf
-firewall_conf
-wg_conf
-wgui_conf
+main
