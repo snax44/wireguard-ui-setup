2e8a03d9a4e7b6a195fcf27ce8e0809e9152a515
Mael Jeuffrard <mj@ilatan.fr>
2021-04-01 18:54:53 +0200

    some comment

--- install.sh
+++ install.sh
@@ -27,21 +27,22 @@
 ###
 
 if ! [ $(id -nu) == "root" ]; then
-  echo "Please run this script as root"
+  echo "Oops ! Please run this script as root"
   exit 1
 fi
 
 if [ "$(lsb_release -is)" != "Debian" ] && [ "$(lsb_release -rs)" != "10" ]
   then
-    echo "This script was made for on Debian10 only."
+    echo "Oops ! This script was tested on Debian10 only."
     exit 1
 fi
 
+# Link to the last release
 WGUI_LINK="https://github.com/ngoduykhanh/wireguard-ui/releases/download/v0.2.7/wireguard-ui-v0.2.7-linux-amd64.tar.gz"
-WGUI_PATH="/opt/wgui"
-WGUI_BIN_PATH="/usr/local/bin"
+WGUI_PATH="/opt/wgui"                                                     # Where Wireguard-ui will be install
+WGUI_BIN_PATH="/usr/local/bin"                                            # Where the symbolic link will be make
 SYSTEMCTL_PATH="/usr/bin/systemctl"
-BACKPORTS_REPO="deb https://deb.debian.org/debian/ buster-backports main"
+BACKPORTS_REPO="deb https://deb.debian.org/debian/ buster-backports main" # It's needed for Debian10, leave it blank for Debian11 (BACKPORTS_REPO="")
 
 function main() {
 
@@ -100,10 +101,12 @@ echo ""
 
 function install() {
 
-  if ! grep -q "^$BACKPORTS_REPO" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
-    echo ""
-    echo "### Enable debian-backports"
-    echo "deb https://deb.debian.org/debian/ buster-backports main" >> /etc/apt/sources.list
+  if [[ ! $BACKPORTS_REPO == "" ]]; then
+    if ! grep -q "^$BACKPORTS_REPO" /etc/apt/sources.list /etc/apt/sources.list.d/*; then
+      echo ""
+      echo "### Enable Backports"
+      echo $BACKPORTS_REPO >> /etc/apt/sources.list
+    fi
   fi
 
   echo ""
@@ -142,6 +145,7 @@ function firewall_conf() {
     mkdir -m 755 /etc/iptables
   fi
 
+  # Backup actual firewall configuration
   /sbin/iptables-save > /etc/iptables/rules.v4.bak
   /sbin/ip6tables-save > /etc/iptables/rules.v6.bak
 
@@ -155,6 +159,10 @@ function firewall_conf() {
   "FORWARD -s $WG_NETWORK -i $WG_INTERFACE -o $SYS_INTERFACE -m comment --comment Wireguard-traffic-from-$WG_INTERFACE-to-$SYS_INTERFACE -j ACCEPT"
   "FORWARD -d $WG_NETWORK -i $SYS_INTERFACE -o $WG_INTERFACE -m comment --comment Wireguard-traffic-from-$SYS_INTERFACE-to-$WG_INTERFACE -j ACCEPT"
   "FORWARD -d $WG_NETWORK -i $WG_INTERFACE -o $WG_INTERFACE -m comment --comment Wireguard-traffic-inside-$WG_INTERFACE -j ACCEPT"
+  "FORWARD -p tcp --syn -m limit --limit 1/second -m comment --comment Flood-&-DoS -j ACCEPT"
+  "FORWARD -p udp -m limit --limit 1/second -m comment --comment Flood-&-DoS -j ACCEPT"
+  "FORWARD -p icmp --icmp-type echo-request -m limit --limit 1/second -m comment --comment Flood-&-DoS -j ACCEPT"
+  "FORWARD -p tcp --tcp-flags SYN,ACK,FIN,RST RST -m limit --limit 1/s -m comment --comment Port-Scan -j ACCEPT"
   "OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT"
   "OUTPUT -o lo -m comment --comment localhost-network -j ACCEPT"
   "OUTPUT -o $WG_INTERFACE -m comment --comment wireguard-network -j ACCEPT"
@@ -174,6 +182,7 @@ function firewall_conf() {
   "OUTPUT -o lo -m comment --comment localhost-network -j ACCEPT"
   )
 
+  # Apply rules only if they are not already present
   for e in "${RULES_4[@]}"; do
     iptables -C $e > /dev/null 2>&1 || iptables -A $e
   done
@@ -181,6 +190,7 @@ function firewall_conf() {
     ip6tables -C $e > /dev/null 2>&1 || ip6tables -A $e
   done
 
+  # Change default policy to DROP instead ACCEPT
   iptables -P INPUT DROP
   iptables -P FORWARD DROP
   iptables -P OUTPUT DROP
@@ -188,9 +198,11 @@ function firewall_conf() {
   ip6tables -P FORWARD DROP
   ip6tables -P OUTPUT DROP
 
+  # Backup allrules (old and new)
   /sbin/iptables-save > /etc/iptables/rules.v4
   /sbin/ip6tables-save > /etc/iptables/rules.v6
 
+  # Make a script for a persistent configuration
   echo "#!/bin/sh
   /sbin/iptables-restore < /etc/iptables/rules.v4
   /sbin/ip6tables-restore < /etc/iptables/rules.v6" > /etc/network/if-up.d/iptables
